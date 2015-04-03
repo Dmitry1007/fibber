6a1e4187f544cb152a169b947db7c8c204483fb3
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-03 11:40:02 -0600

    completed challenge two, better_output :)

--- fibber.rb
+++ fibber.rb
@@ -20,14 +20,39 @@ class Fibber
   def jagged_output(fibo)
     nested = []
     fibo.each_slice(5) { |e| nested << e }
+    nested.each        { |a,b,c,d,e| print "#{a} #{b} #{c} #{d} #{e}\n" }
+  end
+
+  def better_output(fibo)
+    nested = []
+    fibo.each_slice(5) { |e| nested << e }
+    a = nested[0]
+    b = nested[1]
+    c = nested[2]
     
-    nested.each { |a,b,c,d,e| puts "#{a} #{b} #{c} #{d} #{e}\n" }
+    puts "        #{a[0]}    #{a[1]}    #{a[2]}    #{a[3]}    #{a[4]}"
+    puts "        #{b[0]}    #{b[1]}   #{b[2]}   #{b[3]}   #{b[4]}"
+    puts "       #{c[0]}   #{c[1]}  #{c[2]}  #{c[3]}  #{c[4]}"
   end
 
 end
 
 fibber = Fibber.new
 fibo = fibber.fiberize(15)
-fibber.jagged_output(fibo)
+# fibber.jagged_output(fibo)
+fibber.better_output(fibo)
+
+# Challenge 1: Jagged Output
+
+# 0 1 1 2 3
+# 5 8 13 21 34
+# 55 89 144 233 377
+
+# Challenge 2: Better Output
+
+#    0    1    1    2    3
+#    5    8   13   21   34
+#   55   89  144  233  377
+
 
 
